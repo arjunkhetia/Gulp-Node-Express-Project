15c8d8a3307298899e3c631560a539f74593676c
Arjun Khetia <arjunkhetia@gmail.com>
2020-01-12 16:23:38 +0530

    Gulp - The Streaming Build System (Task Runner) with Node.Js & Express

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+// A tool to find an open port or domain socket on the machine
+var portfinder = require('portfinder');
+var port = 3000;
+var portSpan = 999;
 
 gulp.task('serve', function() {
-  nodemon({
-    script: 'bin/www',
-    ext: '',
-    env: {
-      NODE_ENV: 'dev',
-      PORT: 3000
-    },
-    ignore: ['./node_modules/**']
+  portfinder.getPort({
+    port: port,    // minimum port number
+    stopPort: port + portSpan // maximum port number
+  }, function (err, openPort) {
+    if (err) throw err;
+    port = openPort;
+    console.log('Server will start on port ' + port);
+    nodemon({
+      script: 'bin/www',
+      ext: '',
+      env: {
+        NODE_ENV: 'dev',
+        PORT: port
+      },
+      ignore: ['./node_modules/**']
+    });
   });
 });
 
